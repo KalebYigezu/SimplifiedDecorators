df4bf83e9711976ad2d9cd46865705c613035340
KalebYigezu <43226925+KalebYigezu@users.noreply.github.com>
2024-09-21 22:26:57 -0700

    Update Decorators.py

--- Decorators.py
+++ Decorators.py
@@ -1,1 +1,33 @@
 
+def mod_div(func):
+    def inner(a, b):
+        if b == 0:
+            print("You can not divide any number by zero")
+        else:
+            print(a/b)
+    return inner
+
+def div(a, b):
+    print(a/b)
+    
+div = mod_div(div)
+
+div(8, 0)
+
+------------------ or -------------------
+
+def mod_div(func):
+    def inner(a, b):
+        if b == 0:
+            print("You can not divide any number by zero")
+        else:
+            print(a/b)
+    return inner
+
+
+@mod_div
+def div(a, b):
+    print(a/b)
+    
+    
+div(8, 0)
